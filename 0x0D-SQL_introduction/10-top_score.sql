0b30a1ea9ba1cee13b00bb56b3a3ac967ff9e3d8
Aaliiyuu <aliuukhtar123@gmail.com>
2024-05-26 11:09:31 +0100

    selection specefic columns from table

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Lists all records of the table second_table.
 -- Records are ordered by descending score.
-SELECT 'name', 'score' FROM second_table ORDER BY 'score' DESC;
+SELECT 'name', 'score' FROM 'second_table' ORDER BY 'score' DESC;
